9a057fa9bd2e3d210ed2c229f5f95c1790473f1a
LEANDRO MACHADO DA SILVA <49540210+Leandrosilva14@users.noreply.github.com>
2019-07-02 21:12:12 -0300

    Update Temperatura.java

--- Trab2Bim/br.com.leandromachado/classesmolde/Temperatura.java
+++ Trab2Bim/br.com.leandromachado/classesmolde/Temperatura.java
@@ -1,1 +1,17 @@
+package br.com.leandromachado.classesmolde;
 
+public class Temperatura {
+    private double grau;
+
+    public Temperatura(double grau) {
+        this.grau = grau;
+    }
+
+    public double getGrau() {
+        return grau;
+    }
+
+    public void setGrau(double grau) {
+        this.grau = grau;
+    }
+}
