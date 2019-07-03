fb1ae60f171bb682fd85b107bf84558189e7d6fb
LEANDRO MACHADO DA SILVA <49540210+Leandrosilva14@users.noreply.github.com>
2019-07-02 21:11:15 -0300

    Update EstacaoRadio.java

--- Trab2Bim/br.com.leandromachado/classesmolde/EstacaoRadio.java
+++ Trab2Bim/br.com.leandromachado/classesmolde/EstacaoRadio.java
@@ -1,1 +1,22 @@
+package br.com.leandromachado.classesmolde;
 
+public class EstacaoRadio {
+    private double sintonia;
+
+    public EstacaoRadio() {
+
+    }
+
+    public EstacaoRadio(double sintonia) {
+        this.sintonia = sintonia;
+    }
+
+    public double getSintonia() {
+        return sintonia;
+    }
+
+    public void setSintonia(double sintonia) {
+        this.sintonia = sintonia;
+    }
+
+}
