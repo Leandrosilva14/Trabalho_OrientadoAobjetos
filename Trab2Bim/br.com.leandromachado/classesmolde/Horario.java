f544e808f6484276d4544aa741d60d9189e86077
LEANDRO MACHADO DA SILVA <49540210+Leandrosilva14@users.noreply.github.com>
2019-07-02 21:11:38 -0300

    Update Horario.java

--- Trab2Bim/br.com.leandromachado/classesmolde/Horario.java
+++ Trab2Bim/br.com.leandromachado/classesmolde/Horario.java
@@ -1,1 +1,29 @@
+package br.com.leandromachado.classesmolde;
 
+public class Horario {
+    private int hora;
+    private int minuto;
+
+    public Horario() {}
+
+    public Horario(int hora, int minuto) {
+            this.setHora(hora);
+            this.setMinuto(minuto);
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+}
