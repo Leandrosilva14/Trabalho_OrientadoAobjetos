6470f92f64f3e504837fe60fc4f249f89f4e2c59
LEANDRO MACHADO DA SILVA <49540210+Leandrosilva14@users.noreply.github.com>
2019-07-02 21:09:56 -0300

    Update Calendario.java

--- Trab2Bim/br.com.leandromachado/classesmolde/Calendario.java
+++ Trab2Bim/br.com.leandromachado/classesmolde/Calendario.java
@@ -1,1 +1,42 @@
+package br.com.leandromachado.classesmolde;
 
+public class Calendario {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Calendario() {
+
+    }
+
+    public Calendario(int dia, int mes, int ano) {
+        this.setDia(dia);
+        this.setMes(mes);
+        this.setAno(ano);
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+}
